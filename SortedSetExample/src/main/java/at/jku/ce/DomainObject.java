92393178c94ae3d5a822bc23861d737ae973d820
Arthur <johannes@kutsam.at>
2011-11-28 11:28:45 +0100

    Tag 3: Artur uuid hinzu - und code formatiert Signed-off-by: Arthur <johannes@kutsam.at>

--- SortedSetExample/src/main/java/at/jku/ce/DomainObject.java
+++ SortedSetExample/src/main/java/at/jku/ce/DomainObject.java
@@ -2,20 +2,24 @@ package at.jku.ce;
 
 public class DomainObject {
 
+	public String id; 
+	private String name;
+	public String id = UUID.randomUUID().toString()
+	
 	public DomainObject() {
 		super();
+	}
+	
+	public DomainObject(String id){
+		super();
+		this.id=id;
+	}
+	 
+	public String getId() {
+		return id;
 	}
 
 }
 
-public String id; 
-public DomainObject(String id) 
-{ 
-super();
- this.id = id; 
- }
- 
- public String getId() {
-return id;
-}
- private String name;
+
+
